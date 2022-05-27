eced6e0bc8e7dd9940f374448b047a5d00f8e6f9
Bruno Peres <bbrunopf@gmail.com>
2022-05-27 13:01:10 -0300

    pretty much all done

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function mdc(n1, n2) { // calcula mdc, talvez deletar dps!!!!!!
     return mdc(n2, n1 % n2);
   }
 
-//antigo acima, testando novo abaixo
 
 //exemplo de sistema:
 /*
@@ -18,17 +17,16 @@ function mdc(n1, n2) { // calcula mdc, talvez deletar dps!!!!!!
 
 
 
-function calcInversoMod(a, m) // funcao q calcula o inverso modular
-{
-    for(let x = 1; x < m; x++)
-        if (((a % m) * (x % m)) % m == 1)
-            return x;
+function calcInversoMod(a, m){ // funcao q calcula o inverso modular
+  for(let x = 1; x < m; x++)
+    if (((a % m) * (x % m)) % m == 1)
+      return x;
 }
 
 function calcModulo (poscongr, modulom) { // função q calcula o modulo m
-    let passo1 = Math.floor(poscongr/modulom);
-    let passo2 = passo1 * modulom
-    let passofinal = poscongr - passo2;
+  let passo1 = Math.floor(poscongr/modulom);
+  let passo2 = passo1 * modulom
+  let passofinal = poscongr - passo2;
     return passofinal;
 }// exemplo da funcao: calcModulo(5,3) É A MESMA COISA QUE: x ≡ 5 (mod 3)
 
@@ -63,14 +61,16 @@ let b1 = document.querySelector(".post-x-1").value
 let b2 = document.querySelector(".post-x-2").value
 let b3 = document.querySelector(".post-x-3").value
 
-//BUG \/
-//quando eu coloco outro valor sem atualizar a pagina, ele escreve a mais em baixo, CONSERTAR ISSO
 
 //mod m
 let m1 = document.querySelector(".mod-1").value
 let m2 = document.querySelector(".mod-2").value
 let m3 = document.querySelector(".mod-3").value
 
+if (b1 == "" || b2 == "" || b3 == "" || m1 == "" || m2 == "" || m3 == "") {
+  document.querySelector(".x0").innerHTML = "Campo obrigatório não preenchido!";
+  
+}else{
 //M
 let mz1 = m2 * m3
 let mz2 = m1 * m3
@@ -115,38 +115,20 @@ document.querySelector(".x0").innerHTML = "x0 =  " +  resposta
 //achando a resposta, equacao final sera:
 // resposta + modresp * t
 //t = 1,2,3...
+if(document.querySelector(".solucoes").innerHTML != "" && document.querySelector(".solucoes2").innerHTML != ""){
+  document.querySelector(".solucoes").innerHTML = "";
+  document.querySelector(".solucoes2").innerHTML = "";
+}
 document.querySelector(".psolucoes").innerHTML = "Possíveis soluções:"
-for(let t = 0; t <= 5; t++){ // possiveis respostas
+for(let t = 0; t < 3; t++){ // possiveis respostas
   let finalresp = resposta + (modresp * t)
-  document.querySelector(".solucoes").innerHTML +=  finalresp  + '<br>'
-  console.log(finalresp)
+  document.querySelector(".solucoes").innerHTML += "Para t = "+ t+ ": " + finalresp  + '<br>'
+}
+for(let t = 3; t <= 5; t++){ // possiveis respostas
+  let finalresp = resposta + (modresp * t)
+  document.querySelector(".solucoes2").innerHTML += "Para t = "+ t+ ": " + finalresp  + '<br>'
 }
 
 //equacao geral:
-document.querySelector(".eqgeral").innerHTML = "Equação geral: <br>" + resposta + " + " + modresp + " * t"
-console.log("Equação final:")
-console.log(resposta + " + " + modresp + " * t")
-
-}
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-/*
-function soma(){
-    let input1 = document.querySelector(".pre-x").value;
-    let input2 = document.querySelector(".post-x").value;
-
-    let result = parseInt(input1) + parseInt(input2);
-    document.querySelector(".equacao").innerHTML = input1;
-}*/
+document.querySelector(".eqgeral").innerHTML = "Equação geral: <br>" + resposta + " + " + modresp + " × t"
+}}
